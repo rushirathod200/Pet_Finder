420358ad29f9e549feaca196fd28080bbdc1ccd0
rushi rathod <rushirathod943@gmail.com>
2026-02-17 09:30:41 +0530

    Handle homepage DB errors and enable stderr logging on Render

--- app/Http/Controllers/homepagecontroller.php
+++ app/Http/Controllers/homepagecontroller.php
@@ -5,17 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\pets;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
+use Throwable;
 
 class homepagecontroller extends Controller
 {
     function index()
     {
         $featuredPets = collect();
-        if (Schema::hasTable('pets')) {
-            $featuredPets = pets::where('adopted', false)
-                ->latest('id')
-                ->take(10)
-                ->get();
+        try {
+            if (Schema::hasTable('pets')) {
+                $featuredPets = pets::where('adopted', false)
+                    ->latest('id')
+                    ->take(10)
+                    ->get();
+            }
+        } catch (Throwable $e) {
+            report($e);
         }
 
         return view('homepage', [
